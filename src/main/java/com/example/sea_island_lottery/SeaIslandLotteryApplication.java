a8a1fd16ad72f98aac1814c606a7b32fb4c19c04
Kotaro <ino.kota0602@gmail.com>
2026-03-03 14:14:07 +0900

    chore: SpringBootプロジェクト初期構成

---
+++ src/main/java/com/example/sea_island_lottery/SeaIslandLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sea_island_lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaIslandLotteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaIslandLotteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sea_island_lottery/SeaIslandLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sea_island_lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaIslandLotteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
