0f58fad342532af23a9ac4c0035b42a48b9e8787
K <ino.kota0602@gmail.com>
2026-03-06 07:06:52 +0900

    feat: トップページ/ th:eachでカード繰り返し表示化
    * feat: トップページ作成
    
    * feat: トップページ/ th:eachでカード繰り返し表示化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./src/main/resources/templates/**/*.html",
     "./src/main/resources/static/**/*.js"
